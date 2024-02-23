1e754aa0324c9acbcd65de4fb1ca2673cceeb90a
Blue89Azul <life.89.blue@gmail.com>
2024-02-23 20:22:00 +0900

    modified format

--- test/index.js
+++ test/index.js
@@ -1,17 +1,17 @@
-const assert   = require("assert");
+const assert = require("assert");
 const fizzbuzz = require("../index");
 
 describe("fizzbuzz", () => {
-    it("returns FizzBuzz when value is devisible by 15", () => {
-        assert(fizzbuzz(30) === "FizzBuzz");
-    });
-    it("returns FizzBuzz when value is devisible by 3", () => {
-        assert(fizzbuzz(9) === "Fizz");
-    });
-    it("returns FizzBuzz when value is devisible by 5", () => {
-        assert(fizzbuzz(10) === "Buzz");
-    });
-    it("returns the value when value is not divible by 3 or 5", () => {
-        assert(fizzbuzz(7) === "7");
-    });
+  it("returns FizzBuzz when value is devisible by 15", () => {
+    assert(fizzbuzz(30) === "FizzBuzz");
+  });
+  it("returns FizzBuzz when value is devisible by 3", () => {
+    assert(fizzbuzz(9) === "Fizz");
+  });
+  it("returns FizzBuzz when value is devisible by 5", () => {
+    assert(fizzbuzz(10) === "Buzz");
+  });
+  it("returns the value when value is not divible by 3 or 5", () => {
+    assert(fizzbuzz(7) === "7");
+  });
 });
